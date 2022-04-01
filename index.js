3cf50fc50767f9d13ae52c9fce449751e3afe4d1
Camillo Ferraris <camillo777@gmail.com>
2022-04-01 13:28:29 +0200

    logs

--- index.js
+++ index.js
@@ -4,29 +4,38 @@ main()
 
 async function main() {
 
+    console.log( 'START' )
     try {
-    await getBalance();
+        await getBalance();
     }
     catch( e ) {
         console.error( e )
     }
+    console.log( 'END' )
 
 }
 
 async function getBalance() {
+    console.log( 'getBalance' )
+
+    console.log( 'creating provider' )
     var provider = new ethers.providers.JsonRpcProvider( 'https://evm-t3.cronos.org:8545' )
 
-    await provider.getBlockNumber()
+    console.log( 'getting block number' )
+    var blockNumber = await provider.getBlockNumber()
     // 14467379
+    console.log( 'blockNumber', blockNumber )
 
+    console.log( 'getting balance' )
     // Get the balance of an account (by address or ENS name, if supported by network)
     balance = await provider.getBalance("ethers.eth")
     // { BigNumber: "82826475815887608" }
 
     // Often you need to format the output to something more user-friendly,
     // such as in ether (instead of wei)
-    ethers.utils.formatEther(balance)
+    var fBalance = ethers.utils.formatEther(balance)
     // '0.082826475815887608'
+    console.log( 'fBalance', fBalance )
 
     // If a user enters a string in an input field, you may need
     // to convert it from ether (as a string) to wei (as a BigNumber)
